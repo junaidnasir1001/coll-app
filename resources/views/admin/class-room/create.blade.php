2896755e3faa3024d6603761f420fe4d5f082a14
hamzaazmat007 <79515237+hamzaazmat007@users.noreply.github.com>
2022-03-07 07:10:12 -0800

    deesign
    design

--- resources/views/admin/class-room/create.blade.php
+++ resources/views/admin/class-room/create.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Add Class Room</h1>
+                        <h1>Add Class Rooms</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
